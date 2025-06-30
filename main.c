785a0c074b61734186d54458a1019797b19697f4
Rahat624Imran <rahatemran06@gmail.com>
2025-06-30 22:45:27 +0600

    Update main.c

--- main.c
+++ main.c
@@ -10,7 +10,8 @@ int main()
     int is_word_valid;
     char word[MAX];
 
-
+    printf("Let's play Hangman!");
+    printf("Choose who you would like to be Player 1 and Player 2");
     while (1)
     {
       is_word_valid = 1;
@@ -39,6 +40,26 @@ int main()
 
         }
         }
+        if (is_word_valid)
+            {
+              break;
+            }
+
+
+    }
+
+
+    }
+    for (int i=0; i<50; i++)
+        {
+        printf("\n");
+        }
+    printf("Word to guess: ");
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        printf("_ ");
+    }
+    printf("\n");
 
 
 }
